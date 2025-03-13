52f486d9ccd565e3581f8b667543a67229f2c0ad
unknown <f.chagasdutra18@gmail.com>
2025-03-12 22:58:06 -0300

    dom-manipulaiton-in-Javascript

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
 const wrapper = document.querySelector(".sliderWrapper")
 const menuItems = document.querySelectorAll(".menuItem")
 
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png"
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png"
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png"  
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Air Forcem",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png"
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png"  
+      },
+    ],
+  }
+]
+
+
+
+
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
