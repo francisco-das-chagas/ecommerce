2d03295c312a1116c9c745ab7ccaaa2dbfbea683
unknown <f.chagasdutra18@gmail.com>
2025-03-15 20:08:52 -0300

    dom-manipulation-in-javascript

--- app.js
+++ app.js
@@ -34,26 +34,74 @@ const products = [
   },
   {
     id: 3,
-    title: "Air Forcem",
-    price: 119,
+    title: "Blazer",
+    price: 109,
     colors: [
       {
-        code: "black",
-        img: "./img/air.png"
+        code: "lightgray",
+        img: "./img/blazer.png"
       },
       {
         code: "darkblue",
-        img: "./img/air2.png"  
+        img: "./img/blazer2.png"  
       },
     ],
-  }
-]
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png"
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png"  
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png"
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png"  
+      },
+    ],
+  },
+];
 
-
-
+let choosenProduct = products[0]
+const currentProductImg = document.querySelector(".productImg")
+const currentProductTitle = document.querySelector(".productTitle")
+const currentProductPrice = document.querySelector(".productPrice")
+const currentProductColors = document.querySelectorAll(".color")
+const currentProductSizes = document.querySelectorAll(".size")
 
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
+    //change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index]
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title
+    currentProductPrice.textContent = "$" + choosenProduct.price
+    currentProductImg.src = choosenProduct.colors[0].img
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
   });
 });
